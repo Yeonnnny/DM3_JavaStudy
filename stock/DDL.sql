2457fd8e57b9e5f1d953531d9c115c640f1bd824
doyeon <kdoy030@gmail.com>
2024-02-16 09:33:18 +0900

    240216 4th commit : Products라는 이름으로 테이블 생성하려다가 테이블이 이미 존재해서 stock이라는 이름으로 테이블 생성함

---
+++ stock/DDL.sql
@@ -0,0 +1,12 @@
+drop table stock;
+drop SEQUENCE stock_seq;
+
+create table stock(
+    pid number constraint stock_id_pk primary key
+    ,pname  varchar2(100) constraint stock_name_nn not null
+    ,unit_price number constraint stock_price_nn not null
+    ,pnum number default 1
+    ,category varchar2(10) -- enum 타입
+);
+
+create sequence stock_seq;
