cd9ee527f73a4a1276ea259175ce2cb67836a226
doyeon <kdoy030@gmail.com>
2024-02-16 09:30:43 +0900

    240216 3rd commit : oracle DB 연결 관련 util 패키시 생성 및 connection 클래스 생성 및 구현

---
+++ stock/src/main/java/com/miniproject/stock/util/ConnectionManager.java
@@ -0,0 +1,22 @@
+package com.miniproject.stock.util;
+
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.EntityManagerFactory;
+import jakarta.persistence.Persistence;
+
+public class ConnectionManager {
+    private static EntityManagerFactory factory;
+
+    static{
+        factory = Persistence.createEntityManagerFactory("javastudy");
+    }
+
+    public static EntityManager getManager(){
+        return factory.createEntityManager();
+    }
+
+    public static void close(){
+        factory.close();
+    }
+
+}
