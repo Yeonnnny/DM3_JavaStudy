d8a0bae67d14945e8640f222a4afb766764ed798
doyeon <kdoy030@gmail.com>
2024-02-16 09:28:58 +0900

    240216 2nd commit : entity 패키지 생성 및 stock 테이블의 Entity 클래스인 Product구현

---
+++ stock/src/main/java/com/miniproject/stock/entity/Category.java
@@ -0,0 +1,18 @@
+package com.miniproject.stock.entity;
+
+public enum Category {
+    OUTER("아우터"), TOP("상의"), BOTTOM("하의"), ACC("잡화"), SHOES("신발");
+    // 아우터("OUTER"), 상의("TOP"), 하의("BOTTOM"), 잡화("ACC"), 신발("SHOES");
+
+
+    private String category;
+
+    private Category(String category){
+        this.category = category;
+    }
+
+    public String geCategory(){
+        return this.category;
+    }
+
+}
---
+++ stock/src/main/java/com/miniproject/stock/entity/Product.java
@@ -0,0 +1,61 @@
+package com.miniproject.stock.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import jakarta.persistence.SequenceGenerator;
+import jakarta.persistence.Table;
+import lombok.Setter;
+import lombok.ToString;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor
+@Setter
+@Getter
+@ToString
+
+@Entity
+@Table(name="stock") 
+
+public class Product {
+
+    @SequenceGenerator(    
+        name = "stock_seq_gen",
+        sequenceName = "stock_seq",
+        initialValue = 1,
+        allocationSize = 1
+    )
+    @Id
+    @GeneratedValue(generator = "stock_seq_gen")
+    private long pid;
+    
+    @Column(name="pname", nullable = false)
+    private String pname;
+    
+    @Column(name="unit_price", nullable = false)
+    private int price ;
+
+    @Column(name ="pnum")
+    private int stock;
+    
+    @Column(name ="category")
+    @Enumerated (EnumType.STRING)
+    private Category category;
+
+    public Product(String pname, int price, int stock, Category category) {
+        this.pname = pname;
+        this.price = price;
+        this.stock = stock;
+        this.category = category;
+    }
+}
+
+// pid number constraint stock_id_pk primary key
+// ,pname  varchar2(100) constraint stock_name_nn not null
+// ,unit_price number constraint stock_price_nn not null
+// ,pnum number default 1
+// ,category varchar2(10) -- enum 타입
