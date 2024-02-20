7fd52e50e0b8e9fc0e54b76b85a46a0dfc55671c
doyeon <kdoy030@gmail.com>
2024-02-20 18:44:07 +0900

    240220 7th commit : 함수 설명을 위한 document 생성

--- stock/src/main/java/com/miniproject/stock/service/StockService.java
+++ stock/src/main/java/com/miniproject/stock/service/StockService.java
@@ -6,16 +6,47 @@ import com.miniproject.stock.entity.Stock;
 
 public interface StockService {
 
+    /**
+     * pid(시퀀스)로 DB에서 물품 레코드 검색해서 반환하는 함수
+     * @param pid : 물품번호
+     * @return stock : 물품번호에 해당하는 물품
+     */
     public Stock search(long pid);
 
+    /**
+     * DB에 물품 등록하는 함수
+     * @param stock
+     * @return
+     */
     public boolean insert(Stock stock);
 
+    /**
+     * pname(물품명)으로 DB에서 물품 레코드를 검색해서 반환하는 함수
+     * @param pname : 물품명
+     * @return stock : 물품명에 해당하는 물품 
+     */
     public Stock selectOne(String pname);
     
+    /**
+     * DB에 존재하는 모든 물품 리스트로 반환하는 함수
+     * @return 모든 물품 정보
+     */
     public List<Stock> selectAll();
 
-    public boolean updateStock(long pid, int num); // 입고 시 양수, 출고 시 음수 전달
+    /**
+     * 물품명과 변경하고자 하는 수량을 입력받아 물품 정보를 업데이트하는 재고량 수정 함수. 입고 시는 양수로, 출고 시는 음수로 값을 전달. 
+     * @param pname
+     * @param num
+     * @return
+     */
+    public boolean updateStock(String pname, int num); // 입고 시 양수, 출고 시 음수 전달
     
-    public boolean updatePrice(long pid, int price); // 변경할 가격 전달
+    /**
+     * 물품명과 수정하고자 하는 가격을 입력받아 물품 정보 업데이트하는 가격 수정 함수.
+     * @param pname
+     * @param price
+     * @return
+     */
+    public boolean updatePrice(String pname, int price); // 변경할 가격 전달
 
 }
