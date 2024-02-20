8a14a4a313a37384e97a94e612a83499f243d19e
doyeon <kdoy030@gmail.com>
2024-02-20 18:13:29 +0900

    240220 2nd commit : 물품 등록 구현

--- stock/src/main/java/com/miniproject/stock/service/StockServiceImpl.java
+++ stock/src/main/java/com/miniproject/stock/service/StockServiceImpl.java
@@ -1,26 +1,43 @@
 package com.miniproject.stock.service;
 
 import java.util.List;
-
+   
 import com.miniproject.stock.entity.Stock;
+import com.miniproject.stock.util.ConnectionManager;
+
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.EntityTransaction;
 
 public class StockServiceImpl implements StockService{
 
 
     @Override
     public Stock search(int pid) {
-
-
+        
         return null;
     }
 
     @Override
-    public boolean insert(Stock product) {
-        return false;
+    public boolean insert(Stock stock) {
+        boolean result = false;
+        EntityManager manager = ConnectionManager.getManager();
+        EntityTransaction tx = manager.getTransaction();
+        try {
+            tx.begin();
+            manager.persist(stock);
+            tx.commit();
+            result = true;
+        } catch (Exception e) {
+            tx.rollback();
+        }finally{
+            manager.close();
+        }
+        return result;
     }
 
     @Override
     public Stock selectOne(int pid) {
+        
         return null;
     }
 
@@ -30,12 +47,16 @@ public class StockServiceImpl implements StockService{
     }
 
     @Override
-    public boolean updateStock(int pid, int stock) {
+    public boolean updateStock(int pid, int num) {
         return false;
     }
 
     @Override
     public boolean updatePrice(int pid, int price) {
+
+
+
+
         return false;
     }
     
