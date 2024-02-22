9052ba832f93b8c30c64d7157ecfe0148a3385fb
doyeon <kdoy030@gmail.com>
2024-02-22 10:57:22 +0900

    240222 1st commit : 사용하지 않은 import코드 한 줄 제거

--- stock/src/main/java/com/miniproject/stock/entity/Stock.java
+++ stock/src/main/java/com/miniproject/stock/entity/Stock.java
@@ -7,7 +7,6 @@ import jakarta.persistence.Enumerated;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
 import jakarta.persistence.SequenceGenerator;
-import jakarta.persistence.Table;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
