3db5c594359968ed0535897657f01d6476e98790
Yeonnnny <kdoy030@gmail.com>
2024-03-24 22:33:42 +0900

    240324 1st : movie 프로젝트 재생성 및 테이블 구성

---
+++ movie/DDL.sql
@@ -0,0 +1,49 @@
+-- 영화정보 table
+drop table movie;
+drop sequence movie_seq;
+
+create table movie (
+    movie_num number primary key,
+    movie_title varchar2(30) not null,
+    movie_director varchar2(20) not null,
+    movie_genre varchar2(20) not null,
+    average_grade number(3,1),
+    release_date date not null
+);
+
+create sequence movie_seq;
+
+select * from movie;
+
+
+-- 회원 table
+drop table mem;
+drop sequence mem_seq;
+
+create table mem (
+    mem_num number primary key,
+    mem_id varchar2(20) not null unique,
+    mem_pwd varchar2(20) not null
+);
+create sequence mem_seq;
+
+select * from mem;
+
+
+-- 관람평 table
+drop table review;
+drop sequence review_seq;
+
+create table review (
+    review_num number primary key,
+    movie_num number references movie(movie_num) on delete cascade,
+    mem_num number references mem(mem_num) on delete cascade,
+    review_text varchar2(1000) not null,
+    grade number not null,
+    write_date date default sysdate
+);
+
+create sequence review_seq;
+
+select * from review;
+
--- movie/src/main/java/com/javastudy/movie/controller/MainController.java
+++ movie/src/main/java/com/javastudy/movie/controller/MainController.java
@@ -1,13 +1,13 @@
-package com.javastudy.controller;
+package com.javastudy.movie.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
-
 @Controller
-public class MainContoller {
-    @GetMapping({"/",""})
+public class MainController {
+    @GetMapping({ "/", "" })
     public String index() {
         return "index";
     }
+
 }
